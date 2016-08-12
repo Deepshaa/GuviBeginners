595c09cfc9834badb5ca4b7affb404d353b6294f
Deepshaa <deepika.mcse@ksrce.ac.in>
2016-08-12 12:22:34 +0530

    Create OddNum.java

---
+++ OddNum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddNum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the range(as 2 numbers)");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		for(int i=a;i<=b;i=i+2){
+			if(i%2==0){
+				System.out.println(i+1);
+			}
+			else{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
