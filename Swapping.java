5df0b723651ccb1222cd53dcfb42bc9981d7da72
Deepshaa <deepika.mcse@ksrce.ac.in>
2016-08-18 16:05:52 +0530

    Create Swapping.java

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+public class Swapping {
+
+		public static void main(String[] args) {
+		int x=23;
+		int y=51;
+		System.out.println("Before swapping");
+		System.out.println("x="+x+" y="+y);
+		int temp=x;
+		x=y;
+		y=temp;
+		System.out.println("After swapping");
+		System.out.println("x="+x+" y="+y);
+	}
+
+}
